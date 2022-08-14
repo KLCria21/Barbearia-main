44c186086edd178fd218d164c7ee59c4544ee3e1
Daniel-eteot <92882143+Daniel-eteot@users.noreply.github.com>
2022-08-14 20:15:06 -0300

    validação "completa"

--- Barbearia-main/js/script.js
+++ Barbearia-main/js/script.js
@@ -5,16 +5,18 @@ function validar()
 
     if(nome.value == ""){
         alert("nome não informado");
-
         nome.focus();
+      
+
     }
 
   var email = formulario.email;
 
     if(email.value == ""){
         alert("email não informado");
-
         email.focus();
+       
+
     }
 
   var cel = formulario.cel;
@@ -22,16 +24,18 @@ function validar()
 
     if(cel.value == ""){
         alert("Número de celular não informado");
-
         cel.focus();
+
     }
+
+    
 
   var senha = formulario.senha;
 
     if(senha.value == ""){
         alert("Senha não informada");
-
         senha.focus();
+        
     }
 
   var Csenha = formulario.Csenha;
@@ -53,8 +57,56 @@ function validar()
    
 }
 
+function mascara_tel(obj){
+      if(obj.value.length == 0){
+          obj.value += "(";
+      }
+      if(obj.value.length == 3){
+          obj.value += ")";
+      }
+      if(obj.value.length == 8){
+          obj.value += "-";
+      }
+      
+  
+}
 
 
+function somente_numero(e){
+  tecla = (window.event)?event.keyCode:e.which;
+  
+  if((tecla>=48 && tecla<=57) || (tecla>=96 && tecla<=105) || (tecla == 8) || (tecla == 37) || (tecla == 39) || (tecla == 46) ){
+     return true; 
+  }
+  else{
+     return false; 
+  }
+}
+
+var email = formulario.email;
+function validacaoEmail(field){
+  usuario = field.value.substring(0, field.value.indexOf("@"));
+  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+  if ((usuario.length >=1) &&
+      (dominio.length >=3) &&
+      (usuario.search("@")==-1) &&
+      (dominio.search("@")==-1) &&
+      (usuario.search(" ")==-1) &&
+      (dominio.search(" ")==-1) &&
+      (dominio.search(".")!=-1) &&
+      (dominio.indexOf(".") >=1)&&
+      (dominio.lastIndexOf(".") < dominio.length - 1)) {
+  document.getElementById("msgemail").innerHTML="E-mail válido";
+  alert("email invalido");
+  }
+
+  else{
+    document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+    alert("E-mail invalido");
+    }
+
+  }
+  
 
 
 
